ac0b3732dfc2d83198431c000987a78b7957e507
Valery Raraykin <vand_er@mail.ru>
2024-01-10 12:14:22 +0300

    SubnetCalculator теперь возвращает объект IP

--- subnetips.py
+++ subnetips.py
@@ -16,11 +16,19 @@ import ip_conv
 
 
 class IP:
-    def __init__(self, ip: str) -> None:
+    def __init__(self, ip: str, mask: str = "") -> None:
         self.ip_str: str = ip
+        self.mask: str = mask
         self.intoctets: list[int] = ip_conv.str2intoc(ip)
         self.boctets: list[bitarray] = ip_conv.intoc2boc(self.intoctets)
         self.bits: bitarray = ip_conv.boc2bits(self.boctets)
+
+    def __repr__(self):
+        repr: str = self.ip_str
+        if self.mask:
+            repr += "/" + self.mask
+
+        return repr
 
 
 class SubnetCalculator:
@@ -28,36 +36,46 @@ class SubnetCalculator:
         self.ip: IP = ip
         self.mask: int = int(mask)
 
+    def create_ip(self):
+        ip_bits: bitarray = self.ip.bits
+        ip_bits[self.mask:] = 0
+        created_ip: IP = IP(ip_conv.bits2strip(ip_bits), str(self.mask))
+
+        return created_ip
+
     @property
-    def subnet_ip(self) -> str:
-        subnet_bits: bitarray = self.ip.bits
-        subnet_bits[self.mask:] = 0
-        subnet_ip: str = ip_conv.bits2strip(subnet_bits)
+    def subnet_ip(self) -> IP:
+        ip_bits: bitarray = self.ip.bits
+        ip_bits[self.mask:] = 0
+        subnet_ip: IP = IP(ip_conv.bits2strip(ip_bits), str(self.mask))
 
         return subnet_ip
 
     @property
-    def first_ip(self) -> str:
+    def first_ip(self) -> list[int]:
         ip_bits: bitarray = self.ip.bits
         ip_bits[self.mask:] = 0
         ip_bits[-1] = 1
+        first_ip: IP = IP(ip_conv.bits2strip(ip_bits), str(self.mask))
 
-        return ip_conv.bits2strip(ip_bits)
+        return first_ip
 
     @property
-    def last_ip(self) -> str:
+    def last_ip(self) -> list[int]:
         ip_bits: bitarray = self.ip.bits
         ip_bits[self.mask:] = 1
         ip_bits[-1] = 0
+        last_ip: IP = IP(ip_conv.bits2strip(ip_bits), str(self.mask))
 
-        return ip_conv.bits2strip(ip_bits)
+        return last_ip
 
     @property
-    def broadcast_ip(self) -> str:
+    def broadcast_ip(self) -> list[int]:
         ip_bits: bitarray = self.ip.bits
         ip_bits[self.mask:] = 1
+        broadcast_ip: IP = IP(ip_conv.bits2strip(ip_bits), str(self.mask))
 
-        return ip_conv.bits2strip(ip_bits)
+        return broadcast_ip
 
     @property
     def ips_number(self) -> int:
@@ -67,9 +85,12 @@ class SubnetCalculator:
 if __name__ == "__main__":
     print(sys.argv[1])
 
+    ip_str: str
+    mask_str: str
+
     ip_str, mask_str = sys.argv[1].split("/")
 
-    ip: IP = IP(ip_str)
+    ip: IP = IP(ip_str, mask_str)
 
     subnet_calculator = SubnetCalculator(ip, mask_str)
     print(subnet_calculator.subnet_ip)
